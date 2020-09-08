283aa2cb2dbcb61db597024aac1b42d92c72e0e1
wd30gsrc <gd28.c.tateno@gmail.com>
2020-09-08 21:18:09 +0900

    implement getting started after webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+// 絶対パスを生成するメソッド【path】
+// __dirname はこのファイルがある現在のディレクトリ名
+// "現在のディレクトリ名" + "dist" を連結して絶対パスに変換している。
+const outputPath = path.resolve(__dirname, 'dist')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: outputPath
+    }
+}
