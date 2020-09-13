22d00e8a461382b9c20ef4a2e4d50a33a5d366c5
wd30gsrc <gd28.c.tateno@gmail.com>
2020-09-13 11:25:22 +0900

    implement babel-loader and react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // 絶対パスを生成するメソッド【path】
 // __dirname はこのファイルがある現在のディレクトリ名
@@ -13,6 +15,16 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.m?jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+                }
+            },
             {
                 test: /\.css$/,
                 use: [
@@ -37,10 +49,20 @@ module.exports = {
                     limit: 2048,
                     name: './images/[name].[ext]'
                 }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
             }
         ]
     },
     devServer: {
         contentBase: outputPath
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 }
